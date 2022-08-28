40ba39e2a1556ec784e921980ff3d5c2a987dac0
brunorolak <bruno.rolak@gmail.com>
2022-08-28 11:09:22 -0300

    ali: update vendorsetup to ali

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,25 +1,30 @@
 # Clone/Fetch Upstream Device Dependencies
 # Device tree
 echo "cloning commmon"
-git clone https://github.com/LucyfMorning/device_xiaomi_sdm845-common device/xiaomi/sdm845-common
+git clone -b lineage-19.1 git@github.com:brunorolak/device_motorola_msm8953-common.git device/motorola/msm8953-common
 echo ""
 
 echo "cloning vendor blobs"
-git clone https://github.com/LucyfMorning/vendor_xiaomi_dipper vendor/xiaomi/dipper
-git clone https://github.com/LucyfMorning/vendor_xiaomi_sdm845common vendor/xiaomi/sdm845-common
+git clone -b lineage-19.1 git@github.com:brunorolak/vendor_motorola_ali.git vendor/motorola/ali
+git clone -b lineage-19.1 git@github.com:brunorolak/vendor_motorola_msm8953-common.git vendor/motorola/msm8953-common
 echo ""
 
 # Kernel
 echo "cloning kernel and clang"
-git clone -b twelve https://github.com/PixelExperience-Devices/kernel_xiaomi_polaris kernel/xiaomi/dipper
-git clone --depth=1 https://gitlab.com/jjpprrrr/prelude-clang prebuilts/clang/host/linux-x86/clang-prelude
+git clone -b lineage-19.1 git@github.com:brunorolak/kernel_motorola_msm8953.git kernel/motorola/msm8953
 echo ""
 
 # HAL's
-echo "removing and cloning Hal's pathc for sdm845"
-rm -rf hardware/qcom-caf/sdm845/display
-rm -rf hardware/qcom-caf/sdm845/media
-git clone https://github.com/PixelExperience/hardware_qcom-caf_sdm845_display hardware/qcom-caf/sdm845/display
-git clone https://github.com/PixelExperience/hardware_qcom-caf_sdm845_media hardware/qcom-caf/sdm845/media
+echo "removing qcom-caf 8996 and cloning Hal's from AEX"
+cd hardware/qcom-caf/msm8996 && rm -rf display &&  rm -rf audio && rm -rf media && cd ../../.. 
+git clone https://github.com/AospExtended/platform_hardware_qcom_audio -b 12.x-caf-msm8996 hardware/qcom-caf/msm8996/audio && 
+git clone https://github.com/AospExtended/platform_hardware_qcom_display -b 12.x-caf-msm8996 hardware/qcom-caf/msm8996/display && 
+git clone https://github.com/AospExtended/platform_hardware_qcom_media -b 12.x-caf-msm8996 hardware/qcom-caf/msm8996/media
+echo ""
+
+# FM Radio App
+echo "Cloning libs and app from Pixel Experience"
+git clone -b twelve https://github.com/PixelExperience/packages_apps_FMRadio packages/apps/FMRadio
+git clone -b twelve https://github.com/PixelExperience/vendor_qcom_opensource_libfmjni vendor/qcom/opensource/libfmjni
 echo ""
 
